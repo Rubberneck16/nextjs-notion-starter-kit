2cc669947fdc345e7d8a1a4a7d7dcde500bbcdf2
Rubberneck16 <98016449+Rubberneck16@users.noreply.github.com>
2023-11-29 12:26:00 -0500

    Update Personal Web

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,14 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Yaoyun Qi Film Works Showcase',
+  domain: 'notion.so/Qi-Yaoyun-4c56ddff58ee4641b83072e16c8aa670',
+  author: 'Yaoyun Qi',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
